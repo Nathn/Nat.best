1875e8b91207b2a684978a899fe83943429aadc6
NathanTrc <nathan@nat.best>
2020-05-02 11:33:03 +0200

    Update

--- index.php
+++ index.php
@@ -15,7 +15,16 @@
 <br>
 
 <h1 align="center">Salut !</h1>
-<h3><div class="contact"><span style="text-decoration: underline; color: #72abf7; position: absolute; top: 15px; left: 32%"> Me contacter</span><br><p>- Par email</p></div></h3>
+<h3><div class="contact"><span style="text-decoration: underline; color: #72abf7; position: absolute; top: 17px; left: 30%"> Me contacter</span><br><br><p>- Par email<br><font size="3"><i>> <a href="mailto:nathan@nat.best">nathan@nat.best</a><br>> <a href="mailto:nathan@tranchant.fr">nathan@tranchant.fr</a></i></font></p>
+<p>- Via formulaire de contact<br><font size="3"><i>
+    <form action="contact.php" method="post">
+		<input type="text" name="nom" value="" placeholder="Nom/pseudo" style="position: absolute; top: 200px; left: 5%; width: 305px; height: 22px; border-radius: 7px"/><br />
+		<input type="text" name="mail" value="" placeholder="E-mail" style="position: absolute; top: 240px; left: 5%; width: 305px; height: 22px; border-radius: 7px" required/><br />
+		<input type="text" name="sujet" value="" placeholder="Sujet" style="position: absolute; top: 280px; left: 5%; width: 305px; height: 22px; border-radius: 7px" required/><br />   
+        <textarea name="message" cols="40" rows="8" placeholder="Message" style="position: absolute; top: 320px; left: 5%; width: 305px; border-radius: 7px" required></textarea><br /><br />
+        <button type="submit" style="position: absolute; top: 480px; left: 30%">Envoyer</button>
+    </form>
+</i></font></p></div></h3>
 <h3 align="center">
     Voici ma page perso, des fois j'y posterai des trucs<br><br>
     <a href="form.php" style="color:#b9a3e3">La page privée (protégée par mot de passe)</a><br><br>
@@ -28,10 +37,15 @@
     <br><br><a href="tests/testage.php">Test : Quel âge avez-vous ?</a>
     </div><br>
 	<div class="boxed">
-    <br><br><a href="ly/index.html">Lose Yourself : Fanpage</a>
+    <br><br><a href="ly/index.html">Projet de NSI :<br>Lose Yourself Fanpage</a>
     </div></div>
 </h3><br><br>
-<font size="4"><div align="center"><b>Pour me soutenir, vous pouvez vous abonner à ma <a href="https://www.youtube.com/NathanT" style="color:#b9a3e3">chaîne Youtube</a><br> (je vise les 1000 abonnés), ou regarder une publicité sur ma <a href="https://utip.io/nathant">page Utip</a></b></div></font>
+<font size="4"><div align="center"><b>Pour me soutenir, vous pouvez vous abonner à ma <a href="https://www.youtube.com/NathanT" style="color:#b9a3e3">chaîne Youtube</a><br> (je vise les 1000 abonnés), ou regarder une publicité sur ma <a href="https://utip.io/nathant">page Utip</a></b>
+
+<br><br><br><br>
+<p>Le code source du site est disponible librement sur <a href="https://github.com/NathanTrc/Nat.best">Github</a> 💜</p>
+
+</div></font>
 
 
 
